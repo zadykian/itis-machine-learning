c4ffa01264b14548b564f66112ebdbdbcca26ea2
zadykian <zadykian@outlook.com>
2020-10-15 12:58:12 +0300

    knp

--- task_5_knp/main.py
+++ task_5_knp/main.py
@@ -20,9 +20,27 @@ def first_connection():
     weight[i_min][j_min] = sys.maxsize
     weight[j_min][i_min] = sys.maxsize
 
+    connect_pnt[i_min] = 1
+    connect_pnt[j_min] = 1
+
 
 def link_all():
-    return -1
+    minim = sys.maxsize
+    i_min, j_min = None, None
+
+    for i in range(n):
+        if connect_pnt[i] == 1:
+            for j in range(0, n):
+                if connect_pnt[j] == 0:
+                    if minim > weight[i][j]:
+                        minim = weight[i][j]
+                        i_min, j_min = i, j
+
+    tree[i_min][j_min] = minim
+    tree[j_min][i_min] = minim
+    weight[i_min][j_min] = weight[j_min][i_min] = sys.maxsize
+    connect_pnt[i_min] = connect_pnt[j_min] = 1
+
 
 
 # число вершин в графе
@@ -36,6 +54,11 @@ for i in range(0, n):
         weight[j][i] = weight[i][j]
 
 tree = [[0 for i in range(n)] for i in range(n)]
+connect_pnt = [0 for i in range(n)]
+
 first_connection()
 
-print(weight)
+while 0 in connect_pnt:
+    link_all()
+
+print(connect_pnt)
