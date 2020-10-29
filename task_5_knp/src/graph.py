3a8463b983d70fefd95d976f0623aec083680c81
zadykian <zadykian@outlook.com>
2020-10-29 17:36:17 +0300

    knp

---
+++ task_5_knp/src/graph.py
@@ -0,0 +1,27 @@
+from typing import List
+
+from graph_edge import GraphEdge
+from graph_node import GraphNode
+from collections.abc import Iterable
+
+
+# Неориентированный граф.
+class Graph:
+
+	def __init__(self):
+		self._edges = []
+
+	# Список рёбер, входящих в состав графа.
+	@property
+	def edges(self) -> List[GraphEdge]:
+		return self._edges
+
+	# Добавить в граф ребро graph_edge.
+	def add_edge(self, graph_edge: GraphEdge):
+		self._edges.append(graph_edge)
+
+	# Определить, содержит ли граф узел graph_node.
+	def contains_node(self, graph_node: GraphNode) -> bool:
+		return any(map(
+			lambda edge: edge.first_node == graph_node or edge.second_node == graph_node,
+			self._edges))
--- task_5_knp/src/knp_algorithm.py
+++ task_5_knp/src/knp_algorithm.py
@@ -2,6 +2,8 @@ from typing import List
 
 from graph_node import GraphNode
 from graph_edge import GraphEdge
+from graph import Graph
+
 
 # Алгоритм кластеризации КНП (поиск кратчайшего незамкнутого пути).
 class KnpAlgorithm:
@@ -11,18 +13,43 @@ class KnpAlgorithm:
 	def __init__(self, required_clusters_number: int):
 		self._required_clusters_number = required_clusters_number
 
-	def perform_clustering(self, points: List[GraphNode]):
-		return 0
+	# Выполнить кластеризацию списка точек и сформировать результат в виде графа.
+	# Граф - лес, состоящий их required_clusters_number компонентов.
+	def perform_clustering(self, points: List[GraphNode]) -> Graph:
+		graph = Graph()
 
-	# Найти среди points две точки, наиболее близко расположенные друг к другу и сформировать ребро GraphEdge
+		# Пока граф не содержит все узлы из points,
+		# ищем минимальное ребро, связывающее новый узел с графом.
+		while all(map(
+				lambda point: graph.contains_node(point),
+				points)):
+
+			min_edge_to_add = self.get_edge_with_min_weight(
+				points,
+				# Ищем ребро, один узел которого находится в графе, а второй - ещё нет.
+				lambda edge:
+					graph.contains_node(edge.first_node) and not graph.contains_node(edge.second_node)
+					or not graph.contains_node(edge.first_node and graph.contains_node(edge.second_node)))
+
+			graph.add_edge(min_edge_to_add)
+
+		return graph
+
+	# Найти среди points две точки, наиболее близко расположенные друг к другу,
+	# и сформировать соединяющее их ребро GraphEdge.
 	@staticmethod
-	def get_edge_with_min_weight(points: List[GraphNode]) -> GraphEdge:
+	def get_edge_with_min_weight(
+			# Список всех точек (узлов графа) для кластеризации.
+			points: List[GraphNode],
+			# Предикат для поиска ребра.
+			edge_predicate) -> GraphEdge:
+
 		min_edge = GraphEdge(points[0], points[1])
 
 		for outer_point in points:
 			for inner_point in filter(lambda point: point != outer_point, points):
 				current_edge = GraphEdge(outer_point, inner_point)
-				if current_edge.weight < min_edge.weight:
+				if edge_predicate(current_edge) and current_edge.weight < min_edge.weight:
 					min_edge = current_edge
 
 		return min_edge
