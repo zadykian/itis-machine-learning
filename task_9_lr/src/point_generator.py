7b187ace5a7930f5a951d823e8f84fc24e3362ee
zadykian <zadykian@outlook.com>
2020-12-14 22:46:12 +0300

    point_generator

---
+++ task_9_lr/src/point_generator.py
@@ -0,0 +1,19 @@
+from typing import Iterable
+from random import (seed, random)
+from point import Point
+
+# Генератор точек.
+class PointGenerator:
+
+    # Сгенерировать points_count случайных точек в трехмерном пространстве.
+    # Диапазон по всем осям - [0..1].
+    @staticmethod
+    def generate_random_points(points_count: int) -> Iterable[Point]:
+        seed()
+        def random_value(): return round(random(), 3)
+
+        return \
+            [
+                Point(random_value(), random_value(), random_value())
+                for _ in range(1, points_count)
+            ]
