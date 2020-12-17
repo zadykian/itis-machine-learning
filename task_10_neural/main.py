b59f695586a6db1b21ae66f3b17e4f1131dff75e
zadykian <zadykian@outlook.com>
2020-12-17 22:28:27 +0300

    neural

--- task_10_neural/main.py
+++ task_10_neural/main.py
@@ -1,83 +1,34 @@
-import numpy as np
-import random
 import pygame
-import tensorflow as tf
-from keras.models import load_model
 from enum import Enum
-from PIL import Image
-from tensorflow.keras import Sequential
-from tensorflow.keras.layers import Flatten, Dense
 
-WHITE = (255, 255, 255)
-BLACK = (0, 0, 0)
-GREEN = (0, 255, 0)
-dbname = 'mnist.keras'
-
+from colors import Colors
+from digit_recognizer import DigitRecognizer
 
 class Button(Enum):
     Left = 1
     Right = 3
 
+def main():
+    screen = pygame.display.set_mode((256, 256))
+    digit_recognizer = DigitRecognizer()
 
-try:
-    f = open(dbname)
-    f.close()
-    print('reading db from file')
-    model = load_model(dbname)
-except IOError:
-    print('creating db')
-    mnist = tf.keras.datasets.mnist
-    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    while True:
+        for i in pygame.event.get():
+            if i.type == pygame.QUIT:
+                pygame.quit()
+                break
+            if i.type == pygame.MOUSEBUTTONDOWN:
+                if i.button == Button.Right.value:
+                    digit_recognizer.recognize_digit(screen)
+            elif i.type == pygame.KEYDOWN and i.key == pygame.K_SPACE:
+                screen.fill(Colors.black)
 
-    x_train = tf.keras.utils.normalize(x_train, axis=1)
-    x_test = tf.keras.utils.normalize(x_test, axis=1)
-
-    model = Sequential()
-    model.add(Flatten())
-
-    model.add(Dense(256, activation=tf.nn.relu))
-    model.add(Dense(256, activation=tf.nn.relu))
-    model.add(Dense(10, activation=tf.nn.softmax))
-
-    model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
-
-    model.fit(x=x_train, y=y_train, epochs=16, batch_size=32)
-
-    model.save(dbname)
-
-print(model.summary())
+        position = pygame.mouse.get_pos()
+        pressed = pygame.mouse.get_pressed()
+        if pressed[0]:
+            pygame.draw.circle(screen, Colors.white, (position[0], position[1]), 8)
+        pygame.display.update()
 
 
-def save_picture_and_predict(screen):
-    path = 'digit.png'
-    pygame.image.save(screen, path)
-    img = Image.open(path)
-    img = img.resize((28, 28))
-    img = img.convert('L')
-    img = np.array(img)
-    img = img.reshape(1, 28, 28, 1)
-    img = img / 255.0
-    prediction = model.predict([img])[0]
-    res = prediction.argsort()[::-1]
-    print(res)
-
-
-screen = pygame.display.set_mode((280, 280))
-line_start = None
-
-while True:
-    for i in pygame.event.get():
-        if i.type == pygame.QUIT:
-            pygame.quit()
-            break
-        if i.type == pygame.MOUSEBUTTONDOWN:
-            if i.button == Button.Right.value:
-                save_picture_and_predict(screen)
-        elif i.type == pygame.KEYDOWN and i.key == pygame.K_SPACE:
-            screen.fill(BLACK)
-    pos = pygame.mouse.get_pos()
-    pressed = pygame.mouse.get_pressed()
-    if pressed[0]:
-        pygame.draw.circle(screen, WHITE,
-                           (pos[0] + random.randrange(2), pos[1] + random.randrange(2)), random.randrange(1, 5))
-    pygame.display.update()
+if __name__ == "__main__":
+    main()
--- task_10_neural/src/digit_recognizer.py
+++ task_10_neural/src/digit_recognizer.py
@@ -1,11 +1,55 @@
+import numpy
+import pygame
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Flatten, Dense
+from pathlib import Path
+import tensorflow
+from keras.models import load_model
+from PIL import Image
 
 # Распознаватель цифр.
 class DigitRecognizer:
 
-    # Произвести обучение модели.
-    def prepare_recognizer(self):
-        raise Exception('not implemented!')
+    def __init__(self):
+        model_file_name = 'neural.keras'
+        self._model = load_model(model_file_name) \
+            if Path(model_file_name).exists() \
+            else DigitRecognizer._create_and_save_model(model_file_name)
+
+    @staticmethod
+    def _create_and_save_model(model_file_name: str):
+        digits_dataset = tensorflow.keras.datasets.mnist
+        (x_train, y_train), (_, _) = digits_dataset.load_data()
+
+        x_train = tensorflow.keras.utils.normalize(x_train, axis=1)
+        # tensorflow.keras.utils.normalize(x_test, axis=1)
+
+        model = Sequential()
+        model.add(Flatten())
+
+        model.add(Dense(256, activation=tensorflow.nn.relu))
+        model.add(Dense(256, activation=tensorflow.nn.relu))
+        model.add(Dense(10, activation=tensorflow.nn.softmax))
+
+        model.compile(
+            optimizer="adam",
+            loss="sparse_categorical_crossentropy",
+            metrics=["accuracy"])
+
+        model.fit(x=x_train, y=y_train, epochs=16)
+        model.save(model_file_name)
+        return model
 
     # Распознать цифру.
     def recognize_digit(self, digit):
-        raise Exception('not implemented!')
+        path = 'digit.png'
+        pygame.image.save(digit, path)
+        img = Image.open(path)
+        img = img.resize((28, 28))
+        img = img.convert('L')
+        img = numpy.array(img)
+        img = img.reshape(1, 28, 28, 1)
+        img = img / 255.0
+        prediction = self._model.predict([img])[0]
+        res = prediction.argsort()[::-1]
+        print(res)
